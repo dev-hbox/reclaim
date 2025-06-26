0cb346dca3b0640d2e90218c27ab1c4bf4e7231e
unknown <dev.hboxdigital@gmail.com>
2025-06-26 22:01:55 +0500

    update reflection triggers array to string with table

--- database/migrations/2025_06_12_174233_create_daily_reflections_table.php
+++ database/migrations/2025_06_12_174233_create_daily_reflections_table.php
@@ -14,18 +14,15 @@ return new class extends Migration
         Schema::create('daily_reflections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->date('date'); // Ensure one record per day per user
+            $table->date('date');
             $table->boolean('is_victory'); // true = victory, false = setback
             $table->unsignedTinyInteger('temptation_level')->nullable(); // scale 0–10
             $table->enum('temptation_label', ['low', 'medium', 'high'])->nullable(); // optional alt label
             $table->enum('time_of_day', ['morning', 'afternoon', 'evening', 'night'])->nullable();
-            $table->json('triggers')->nullable(); // array of selected triggers
-            $table->text('notes')->nullable(); // optional journal field
+            $table->string('triggers')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
-            $table->unique(['user_id', 'date']); // Only one reflection per user per day
-
-
-
+            $table->unique(['user_id', 'date']);
         });
     }
 
